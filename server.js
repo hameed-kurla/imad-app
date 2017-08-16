ec93f215d54c5448ee248ac5765938a6560f5241
Hameed <30654481+hameed-kurla@users.noreply.github.com>
2017-08-16 22:54:58 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,100 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var artcles={
+		artcleOne: {
+					title: `Welcome To Artcle One! Hameed Hussain`,
+					heading:`Artcle One! Web Development`,
+					content: `<p>
+								My name is Hameed and I am your Web Developer. This is the content of my first Article.My name is Hameed and I am your Web Developer. This is the content of my first Article.My name is Hameed and I am your Web Developer. This is the content of my first Article.
+							</p>
+							<p>
+								My name is Hameed and I am your Web Developer. This is the content of my first Article.My name is Hameed and I am your Web Developer. This is the content of my first Article.My name is Hameed and I am your Web Developer. This is the content of my first Article.
+							</p>
+							<p>
+								My name is Hameed and I am your Web Developer. This is the content of my first Article.My name is Hameed and I am your Web Developer. This is the content of my first Article.My name is Hameed and I am your Web Developer. This is the content of my first Article.
+							</p>
+							<p>
+								My name is Hameed and I am your Web Developer. This is the content of my first Article.My name is Hameed and I am your Web Developer. This is the content of my first Article.My name is Hameed and I am your Web Developer. This is the content of my first Article.
+							</p>`
+				},
+		artcleTwo: {
+					title: `Welcome To Artcle Two! Hameed Hussain`,
+					heading:`Artcle Two! Web Development`,
+					content: `<p>
+								My name is Hameed and I am your Web Developer. This is the content of my first Article.My name is Hameed and I am your Web Developer. This is the content of my first Article.My name is Hameed and I am your Web Developer. This is the content of my first Article.
+							</p>
+							<p>
+								My name is Hameed and I am your Web Developer. This is the content of my first Article.My name is Hameed and I am your Web Developer. This is the content of my first Article.My name is Hameed and I am your Web Developer. This is the content of my first Article.
+							</p>
+							<p>
+								My name is Hameed and I am your Web Developer. This is the content of my first Article.My name is Hameed and I am your Web Developer. This is the content of my first Article.My name is Hameed and I am your Web Developer. This is the content of my first Article.
+							</p>
+							<p>
+								My name is Hameed and I am your Web Developer. This is the content of my first Article.My name is Hameed and I am your Web Developer. This is the content of my first Article.My name is Hameed and I am your Web Developer. This is the content of my first Article.
+							</p>`
+				}
+};
+
+function createHtmltemplate(data){
+	var title=data.title;
+	var heading=data.heading;
+	var content=data.content;
+	var htmltemplate=`
+			<!doctype html>
+			<html>
+				<head>
+					<title>
+						${title}
+					</title>
+					<meta name="viewport" content="width-device-width, initial-scale=1">
+					<link href="/ui/style.css" rel="stylesheet" /> 
+				</head>
+				<body>
+					<div class="container">
+						<div class="center">
+							<img src="/ui/madi.png" class="img-medium"/>
+						</div>
+						<div>
+							<a href="/">Home</a>
+						</div>
+						<br>
+						${heading}
+						<div>
+							${content}
+						</div>
+					</div>
+				</body>
+			</html>`;
+	return htmltemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/artcle-two', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'artcle-two.html'));
+});
+
+var counter = 0;
+app.get('/counter', function (req, res) {
+  counter= counter + 1;
+  res.send(counter.toString());
+});
+
+app.get('/getcount', function (req, res) {
+  res.send(counter.toString());
+});
+
+app.get('/:artcleName', function (req, res) {
+	//The Above paramter :artcleName brings artcle name(localhost:8080/artclename)
+	var artcleNameValue = req.params.artcleName;
+	res.send(createHtmltemplate(artcles[artcleNameValue]));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/style.css', function (req, res) {
@@ -21,7 +113,7 @@ app.get('/ui/madi.png', function (req, res) {
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port = 80;
+var port = 8080;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
