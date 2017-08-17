2c2c5453b15ad2ed6de375a75ef14282d4110922
Hameed <30654481+hameed-kurla@users.noreply.github.com>
2017-08-17 23:56:09 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.get('/users', function (req, res) {
 function generatehash(inputString,saltValue){
     
     var returnValue = crypto.pbkdf2Sync(inputString,saltValue,10000,512,'sha512');
-    return returnValue.toString('hex');
+    return [returnValue.toString('hex')].join('$');
 
 }
 
