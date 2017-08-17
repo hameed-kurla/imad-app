cfd8d2ead381ded23ff3f08faba72119f05d5957
Hameed <30654481+hameed-kurla@users.noreply.github.com>
2017-08-17 22:34:21 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.get('/users', function (req, res) {
           res.status(500).send(err.toString());
       }
       else{
-          res.send(JSON.stringify(result.rows));
+          res.send(JSON.stringify(result));
       }
   })
 });
