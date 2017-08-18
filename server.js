244261b383335e13142207d7b3f51b1c3514928c
Hameed <30654481+hameed-kurla@users.noreply.github.com>
2017-08-19 01:16:44 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -220,6 +220,10 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+app.get('/ui/register.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'register.js'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
