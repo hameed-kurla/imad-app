1fbc60e499f7a2c1560601bf1c682a1abecf305d
Hameed <30654481+hameed-kurla@users.noreply.github.com>
2017-08-19 01:18:46 +0400

    [imad-console] Updates ui/register.js

--- ui/register.js
+++ ui/register.js
@@ -1,1 +1,9 @@
 console.log('Aert');
+var username = document.getElementById('username').value;
+var password = document.getElementById('password').value;
+var email = document.getElementById('email').value;
+var submit_btn = document.getElementById('submit_btn');
+
+submit_btn.onclick = function() {
+	console.log('Loaded!');
+};
