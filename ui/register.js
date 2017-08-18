59b78e15f18af6bbc52c8739a97fa7057469e562
Hameed <30654481+hameed-kurla@users.noreply.github.com>
2017-08-19 01:29:58 +0400

    [imad-console] Updates ui/register.js

--- ui/register.js
+++ ui/register.js
@@ -6,4 +6,22 @@ var submit_btn = document.getElementById('submit_btn');
 
 submit_btn.onclick = function() {
 	console.log('Loaded!');
+	var request = new XMLHttpRequest();
+	request.onreadystatechange = function() {
+	    if (request.readyState === XMLHttpRequest.DONE) {
+				if (request.status === 200){
+					alert('User Created Successfully');
+				}
+				else if (request.status === 403){
+				    alert('User not found');
+				}
+				else if (request.status === 500){
+				    alert('Internal Error: something went wrong');
+				}
+	    }			
+	}
+	
+	request.open('POST', 'http://hameedkurla.imad.hasura-app.io/register', true);
+	request.setRequestHeader('Content-Type', 'application/json');
+	request.send(JSON.stringify({username,password,email}));
 };
