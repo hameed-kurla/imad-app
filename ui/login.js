3bdb11dbd1b6abead40262f1d97e0848c1f6a658
Hameed <30654481+hameed-kurla@users.noreply.github.com>
2017-08-19 01:43:11 +0400

    [imad-console] Updates ui/login.js

--- ui/login.js
+++ ui/login.js
@@ -1,1 +1,26 @@
+console.log('Aert');
 
+var submit_btn = document.getElementById('submit_btn');
+
+submit_btn.onclick = function() {
+	console.log('Submit Loaded!');
+	var request = new XMLHttpRequest();
+	request.onreadystatechange = function() {
+	    if (request.readyState === XMLHttpRequest.DONE) {
+				if (request.status === 200){
+					alert('User Created Successfully');
+				}
+				else if (request.status === 403){
+				    alert('User not found');
+				}
+				else if (request.status === 500){
+				    alert('Internal Error: something went wrong');
+				}
+	    }			
+	}
+	var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    request.open('POST', 'http://hameedkurla.imad.hasura-app.io/login', true);
+	request.setRequestHeader('Content-Type', 'application/json');
+	request.send(JSON.stringify({username,password}));
+};
